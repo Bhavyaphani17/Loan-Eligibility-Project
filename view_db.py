63f083ce4568bc7c20c7e4ad2dd19bf9a4dfdb6b
Bhavyaphani17 <vadlamudibhavya2004@gmail.com>
2026-06-24 12:32:53 +0530

    Add files via upload

---
+++ view_db.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+conn = sqlite3.connect('loan_applications.db')
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM applications")
+rows = cursor.fetchall()
+for row in rows:
+    print(row)
+conn.close()
